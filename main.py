38859597e2330a5459867f3b2ef0f3ea05d68b25
Ashutosh Raman <ashutosh.raman@duke.edu>
2021-05-31 21:50:31 -0500

    Added files

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from glob import glob
+import scipy
+
+class NNlib():
+    def __init__(self):
+        self.files = 'folder_name_here'
+
+    def read_files(self):
+        pass
