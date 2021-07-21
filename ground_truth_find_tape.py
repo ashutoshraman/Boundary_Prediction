c15e673933c4fd0415f04b9d718c6daa355b4029
Ashutosh Raman <ashutosh.raman@duke.edu>
2021-07-21 16:47:47 -0400

    started new script for calibration of tape or paper, fo GT establishment

---
+++ ground_truth_find_tape.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from glob import glob
+from numpy.core.fromnumeric import shape
+from numpy.lib.function_base import diff
+import scipy
+
+calibration_folder = '12-15Start_95mA_2tenthsec_2tenthstepsize_4mm_xy/'
+if __name__ == "__main__":
+    pass
